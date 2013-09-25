7223477a561430a2b14a0f9e0d20ddb9123452f6
Megabyte355 <gary.chang.355@gmail.com>
2013-09-25 17:20:38 -0400

    Set destructor of Screen class to be virtual. Removed DLLs in the project folder.

--- DungeonsAndDragons/TitleScreen.h
+++ DungeonsAndDragons/TitleScreen.h
@@ -7,9 +7,9 @@ class TitleScreen : public Screen
 {
 public:
     TitleScreen(void);
-    ~TitleScreen(void);
+    ~TitleScreen(void) override;
 
-    void Instance() override;
+    void Instance();
     void Initialize() override;
     void Update() override;
     void Draw() override;
